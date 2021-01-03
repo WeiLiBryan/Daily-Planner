eb89e8153d7895918043b54a37e6a7102738c07d
William Bryan <wwbryan00@gmail.com>
2021-01-03 05:39:41 -0800

    add storage functionality

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,17 +1,43 @@
-var currentDay = $("#currentDay");
+var currentDay = $("#currentDay"); 
 
-// Inserts a current time into the current time div
+// Inserts current time into the current time div
 var currentTime = function() {
     var currentMoment = moment();
     currentDay.html(currentMoment.format('dddd, MMMM Do YYYY, h:mm:ss a'));
 }
 
-$(document).ready(function(){
+// takes the data value from clicked button then relates it to the corresponding textbox
+// afterward saves it to local storage
+function saveTxt(txtBoxNum) {
+    
+    var text = $('#' + txtBoxNum).val();
+    console.log("Textbox input: " + text);
 
-    // sets time in current time div
+    localStorage.setItem('timeBlock' + txtBoxNum, text);
+
+}
+
+
+$(document).ready(function(){
+    // sets current time in jumbotron
     currentTime();
     // repeats function so that time is always current
     setInterval(currentTime, 1000);
 
-    
+    // For loop checks local storage for data on all boxes
+    for (var i=0; i<9; i++) {
+        var text = localStorage.getItem('timeBlock' + i);
+
+        // If there is value then places text into textbox
+        if (text) {
+            $('#' + i).text(text);
+        }
+    }
+});
+
+// Upon clicking a save button takes clicked button's data then sends it to saveTxt()
+$(".btn-primary").click(function() {
+    var buttonNum = $(this).attr('data-button');
+    console.log("Button Number: " + buttonNum);
+    saveTxt(buttonNum);
 });
