1c74574933af8e2278d105419b5c0bba17f98e7a
William Bryan <wwbryan00@gmail.com>
2021-01-04 01:50:50 -0800

    make blocks change color depending on time

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,8 +1,37 @@
-var currentDay = $("#currentDay"); 
+var currentDay = $("#currentDay");
 
-// Inserts current time into the current time div
+// Gets current date and sets it to storage
+var today = moment().format('LL');
+localStorage.setItem('date', today);
+
+// Retrieves current time and edits page based on that
 var currentTime = function() {
     var currentMoment = moment();
+    var hour = currentMoment.format('HH');
+
+    // For loop decides what color blocks are
+    for (var k=0; k<9; k++) {
+
+        // if the current hour is equal to the data value of time then set class to present
+        if (hour == $('#' + k).attr('data-time')) {
+            $('#' + k).removeClass("past present future");
+            $('#' + k).addClass("present");
+        }
+
+        // If current hour is past data attribute then set class to past
+        else if (hour > $('#' + k).attr('data-time')){
+            $('#' + k).removeClass("past present future");
+            $('#' + k).addClass("past");
+        }
+
+        // If current hour is before data attribute then set class to future
+        else if (hour < $('#' + k).attr('data-time')){
+            $('#' + k).removeClass("past present future");
+            $('#' + k).addClass("future");
+        }
+    }
+
+    // Clock located in jumbotron
     currentDay.html(currentMoment.format('dddd, MMMM Do YYYY, h:mm:ss a'));
 }
 
@@ -28,7 +57,17 @@ function wipe() {
     }
 }
 
+function isNewDay(){
+    // If today is not the same as the date set in storage then clear
+    var newDay = localStorage.getItem('date');
+    if (newDay != today) {wipe()};
+}
+
 $(document).ready(function(){
+
+
+    isNewDay();
+
     // sets current time in jumbotron
     currentTime();
     // repeats function so that time is always current
